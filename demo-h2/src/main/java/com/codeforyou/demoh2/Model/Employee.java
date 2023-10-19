da5f6f16fbf730418193f5af2cdf8ecbb4821df8
Anand <86248882+anand-mm@users.noreply.github.com>
2023-10-19 06:28:26 +0000

    Pending changes exported from your codespace

---
+++ demo-h2/src/main/java/com/codeforyou/demoh2/Model/Employee.java
@@ -0,0 +1,22 @@
+package com.codeforyou.demoh2.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    
+}
---
+++ demo-h2/src/main/java/com/codeforyou/demoh2/Repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.codeforyou.demoh2.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codeforyou.demoh2.Model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+    
+}
